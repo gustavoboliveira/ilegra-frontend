998651bc5abd21bcb7579a85a7abc990b1eb638f
Gustavo Oliveira <gustavo.oliveira@ilegra.com>
2022-02-23 10:33:37 -0300

    create: add theme

---
+++ 04 - Spotify/src/index.js
@@ -0,0 +1,13 @@
+const lib = require("./functions");
+
+const artist = "Arctic Monkeys";
+const playlist = "Top Brasil";
+const track = "Californication";
+
+lib.getTotalTracksInAlbuns(artist)
+lib.getQuantityArtistFollowers(artist)
+lib.getTotalPlaylistResearched(playlist)
+lib.getTotalTrackInPlaylist(playlist)
+lib.getTrackAndArtistsByResearch(track)
+lib.getDurationTotalTracksResearched(track)
+lib.getQuantityTracksResearched(track)
